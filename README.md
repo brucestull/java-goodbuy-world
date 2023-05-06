54f689a014bb6325cb054b94b3fe82c12cef0619
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-06 16:06:18 -0400

    Second Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# java-goodbuy-world
+# Java Goodbuy, World!
+
---
+++ src/GoodbuyWorld.java
@@ -0,0 +1,5 @@
+public class GoodbuyWorld {
+    public static void main(String[] args) {
+        System.out.println("Goodbuy, World! Enjoy the Sails!");
+    }
+}
